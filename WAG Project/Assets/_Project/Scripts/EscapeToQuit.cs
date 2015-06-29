0d4c4e95bab2af6606ecc8ef7aeb52ec400fbb2e
Scott Todd <scott.todd0@gmail.com>
2015-06-28 21:21:22 -0400

    Tested a build, added escape to quit, moved TexturePostProcessor.

---
+++ WAG Project/Assets/_Project/Scripts/EscapeToQuit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeToQuit : MonoBehaviour
+{
+    private void Awake() {
+
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+}
