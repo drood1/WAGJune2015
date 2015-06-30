cb6b21e93beedcda4e3611a0dbc037330f7268b4
Xinoph <b.tam@live.com>
2015-06-30 14:16:26 -0400

    Updated lights on courtyard.

--- WAG Project/Assets/_Project/Scripts/PingPongIntensity.cs
+++ WAG Project/Assets/_Project/Scripts/PingPongIntensity.cs
@@ -6,17 +6,23 @@ public class PingPongIntensity : MonoBehaviour {
     public float low;
     public float high;
     public float duration;
+    public bool randomDuration;
 
     private Light m_Light;
+    private float m_duration;
 
-	// Use this for initialization
-	void Start () {
-	   m_Light = GetComponent<Light>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	   float t = Mathf.PingPong(Time.time, duration);
-       m_Light.intensity = Mathf.Lerp(low, high, t);
-	}
+    // Use this for initialization
+    void Start () {
+        m_Light = GetComponent<Light>();
+        m_duration = duration;
+        if (randomDuration) {
+            m_duration = Random.value * duration + 0.1f;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float t = Mathf.PingPong(Time.time, m_duration);
+        m_Light.intensity = Mathf.Lerp(low, high, t);
+    }
 }
