87258696a0585a052b5d0dd769c56f32ffcfc299
Xinoph <b.tam@live.com>
2015-06-29 23:03:44 -0400

    Obelisk scene. PingPongIntensity script.

---
+++ WAG Project/Assets/_Project/Scripts/PingPongIntensity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongIntensity : MonoBehaviour {
+
+    public float low;
+    public float high;
+    public float duration;
+
+    private Light m_Light;
+
+	// Use this for initialization
+	void Start () {
+	   m_Light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   float t = Mathf.PingPong(Time.time, duration);
+       m_Light.intensity = Mathf.Lerp(low, high, t);
+	}
+}
