e787480484aadaea41df2a447df1ce5953ab0305
Xinoph <b.tam@live.com>
2015-06-30 18:04:51 -0400

    Rocking old lady. Slowed pulsing lights.

---
+++ WAG Project/Assets/PingPongRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongRotation : MonoBehaviour {
+
+    public float minRotation;
+    public float maxRotation;
+    public float duration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   float t = Mathf.PingPong(Time.time, duration) / duration;
+       float newZ = Mathf.Lerp(minRotation, maxRotation, t);
+       transform.rotation = Quaternion.Euler(0, 0, newZ);
+	}
+}
--- WAG Project/Assets/_Project/Scripts/PingPongIntensity.cs
+++ WAG Project/Assets/_Project/Scripts/PingPongIntensity.cs
@@ -6,6 +6,7 @@ public class PingPongIntensity : MonoBehaviour {
     public float low;
     public float high;
     public float duration;
+    public float maxDuration;
     public bool randomDuration;
 
     private Light m_Light;
@@ -16,13 +17,13 @@ public class PingPongIntensity : MonoBehaviour {
         m_Light = GetComponent<Light>();
         m_duration = duration;
         if (randomDuration) {
-            m_duration = Random.value * duration + 0.1f;
+            m_duration = Random.Range(duration, maxDuration);
         }
     }
     
     // Update is called once per frame
     void Update () {
-        float t = Mathf.PingPong(Time.time, m_duration);
+        float t = Mathf.PingPong(Time.time, m_duration) / m_duration;
         m_Light.intensity = Mathf.Lerp(low, high, t);
     }
 }
